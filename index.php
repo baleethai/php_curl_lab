aaf871b390d15b45cd416345c2783564b10f6e03
shivv89 <shivomv89@gmail.com>
2020-10-01 07:35:31 +0530

    Update index.php
    curl ssl version

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $endpointURL);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSLVERSION, 6);
 $result = curl_exec($ch);
 curl_close($ch);
 print_r($result);
